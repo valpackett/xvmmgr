41663072c46d04588790781d34377c7b91d2ac42
Greg V <greg@unrelenting.technology>
2016-08-22 01:54:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# XVMMgr [![unlicense](https://img.shields.io/badge/un-license-green.svg?style=flat)](http://unlicense.org)
+
+![Screenshot](https://i.imgur.com/ZwqYXP1.png)
+
+A simple tray icon program for integrating a UNIX system running on VirtualBox into the Windows desktop:
+
+- at launch, starts [VcXsrv](https://sourceforge.net/projects/vcxsrv/) (at exit, quits it)
+- on left click, starts your VM if it's not started
+- has a right click menu for launching X apps from the VM using `plink`
+
+## Usage
+
+You'll need VcXsrv, VirtualBox, and PuTTY. Generate a private key with PuTTY, save it without encrypting with a password.
+
+First, install the module:
+
+```powershell
+mkdir -Force $HOME\Documents\WindowsPowerShell\Modules\XVMMgr
+cp .\xvmmgr.psm1 $HOME\Documents\WindowsPowerShell\Modules\XVMMgr\XVMMgr.psm1
+```
+
+Then write a script like this:
+
+```powershell
+Import-Module XVMMgr
+
+XVMMgr @{
+    VMName = 'TheVMNameInVirtualBox';
+    SSHHost = 'user@vmhostname-or-ip.lan';
+    SSHPrivateKey = 'C:\Users\user\path\to\unencryptedPrivateKeyForVM.ppk'; # No spaces in the path!
+    Commands = @(
+        @{Name = 'Terminal'; Command = 'st'},
+        @{Name = 'glxgears'; Command = 'glxgears'}
+        # ...
+    )
+}
+```
+
+To run it in the background, you'll need a VBScript file to call your PowerShell script. Yes, this seems to be the easiest way to do this. Oh Windows.
+
+```visualbasic
+Set objShell = WScript.CreateObject("WScript.Shell")
+objShell.Run("powershell.exe -File C:\Users\user\path\to\mgr.ps1"), 0, True
+```
+
+You can put that `.vbs` script into your autostart folder (which is `$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup`, and you can go there quickly by opening `shell:startup` in either the Win+R run dialog or the explorer path bar).
+
+
+By the way: do not install VirtualBox guest OpenGL/X11 drivers.
+
+## License
+
+This is free and unencumbered software released into the public domain.  
+For more information, please refer to the `UNLICENSE` file or [unlicense.org](http://unlicense.org).
---
+++ xvmmgr.psm1
@@ -0,0 +1,51 @@
+﻿Add-Type -AssemblyName "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+Add-Type -AssemblyName "System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+
+function RunPlink ([object]$Config, [string]$cmd) {
+    return Start-Process 'C:\Program Files\VcXsrv\plink.exe' -ArgumentList '-ssh',$Config.SSHHost,'-X','-2','-C','-i',$Config.SSHPrivateKey,$cmd -WindowStyle Hidden -PassThru
+}
+
+function XVMMgr ([object]$Config, [string]$SSHHost, [string]$SSHPrivateKeyPath, [array]$Commands) {
+    Stop-Process -ProcessName VcXsrv -ErrorAction SilentlyContinue
+
+    $VBox = New-Object -ComObject VirtualBox.VirtualBox
+    $VBoxSession = New-Object -ComObject VirtualBox.Session
+    $VBoxBox = $VBox.FindMachine($Config.VMName)
+
+    $Ctx = New-Object System.Windows.Forms.ApplicationContext
+    $Processes = New-Object System.Collections.Generic.List[System.Object]
+    $Processes.Add((Start-Process 'C:\Program Files\VcXsrv\vcxsrv.exe' -ArgumentList '-multiwindow','-wgl','-notrayicon','-xkboptions','compose:ralt' -PassThru))
+
+    $NotifyIcon = New-Object System.Windows.Forms.NotifyIcon
+    $NotifyIcon.Visible = $True
+    $NotifyIcon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon('C:\Program Files\Oracle\VirtualBox\VirtualBox.exe')
+    $NotifyIcon.ContextMenu = New-Object System.Windows.Forms.ContextMenu
+    $NotifyIcon.add_Click({
+        if ($_.Button -eq [System.Windows.Forms.MouseButtons]::Left -and $VBoxBox.State -lt 5) {
+            $VBoxBox.LaunchVMProcess($VBoxSession, 'headless', '')
+        }
+    })
+
+    $MenuItemExit = New-Object System.Windows.Forms.MenuItem
+    $MenuItemExit.Text = "Exit"
+    $MenuItemExit.add_Click({
+        $Ctx.ExitThread()
+    })
+    $NotifyIcon.ContextMenu.MenuItems.AddRange($MenuItemExit)
+
+    foreach ($Cmd in $Config.Commands) {
+        $MenuItem = New-Object System.Windows.Forms.MenuItem
+        $MenuItem.Text = $Cmd.Name
+        $MenuItem.add_Click({
+            $Processes.Add((RunPlink $Config $Cmd.Command))
+        })
+        $NotifyIcon.ContextMenu.MenuItems.AddRange($MenuItem)
+    }
+
+    [System.Windows.Forms.Application]::add_ApplicationExit({
+        foreach ($Proc in $Processes) {
+            Stop-Process $Proc
+        }
+    })
+    [void][System.Windows.Forms.Application]::Run($Ctx)
+}
